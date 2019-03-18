cfaf5b4a88bd3da8faf69e48daa28b7c9418e283
Kate Dameron <katerj33@gmail.com>
2019-03-18 16:25:57 -0700

    random route test

--- test/random.test.js
+++ test/random.test.js
@@ -1,3 +1,16 @@
 const request = require('supertest');
 const app = require('../lib/app.js');
 
+describe('route', () => {
+  it('returns a random ipsum quote', () => {
+    return request(app)
+    .get('/random')
+    .then(res => {
+      expect(res.body).toEqual({
+        text: expect.any(String),
+        __v: 0,
+        _id: expect.any(String)
+      })
+    })
+  });
+});
