bcd4d60e3a0a4b6235499c860c76436c72ce5857
Kate Dameron <katerj33@gmail.com>
2019-03-18 16:21:23 -0700

    seedData

---
+++ test/seedData.js
@@ -0,0 +1,11 @@
+const Random = require('../lib/models/Random');
+const Chance = require('chance');
+const chance = new Chance();
+
+module.exports = function(count = 5) {
+  const quotes = [...Array(count)].map((_, i) => ({
+    text: chance.sentence()
+  }));
+
+  return Random.create(quotes);
+}
